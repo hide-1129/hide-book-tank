a5d4194a5909a51cd1980cde33540eb68e150e17
hide <katohidetaka1129@yahoo.co.jp>
2020-12-07 02:40:27 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,21 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">読書タンク</a>
+        
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <ul class="navbar-nav mr-auto">
+            <li class"nav-item"><a href="#" class="nav-link">検索エンジン</a></li>
+        </ul>
+        
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
